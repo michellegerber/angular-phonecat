f2303de0ba26d5f21bacdd01c8053684f1163018
michellegerber <michellegerber03@gmail.com>
2015-12-17 04:20:41 +0000

    changes

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -74,6 +74,37 @@ describe('PhoneCat App', function() {
 
 
   describe('Phone detail view', function() {
+    describe('PhoneCat App', function() {
+
+  describe('Phone list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+    var phoneList = element.all(by.repeater('phone in phones'));
+    var query = element(by.model('query'));
+
+    it('should filter the phone list as a user types into the search box', function() {
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
+
+    it('should display the current filter value in the title bar', function() {
+      query.clear();
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+      query.sendKeys('nexus');
+      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+    });
+  });
+});
 
     beforeEach(function() {
       browser.get('app/index.html#/phones/nexus-s');
@@ -97,5 +128,30 @@ describe('PhoneCat App', function() {
       element(by.css('.phone-thumbs li:nth-child(1) img')).click();
       expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
     });
+    it('should be possible to control phone order via the drop down select box', function() {
+
+  var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+  var query = element(by.model('query'));
+
+  function getNames() {
+    return phoneNameColumn.map(function(elm) {
+      return elm.getText();
+    });
+  }
+
+  query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+  expect(getNames()).toEqual([
+    "Motorola XOOM\u2122 with Wi-Fi",
+    "MOTOROLA XOOM\u2122"
+  ]);
+
+  element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+  expect(getNames()).toEqual([
+    "MOTOROLA XOOM\u2122",
+    "Motorola XOOM\u2122 with Wi-Fi"
+  ]);
+});
   });
 });
